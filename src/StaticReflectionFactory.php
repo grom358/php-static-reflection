772b612662586376cbcaa94f6a9dc0aef803741e
Cameron Zemek <cameron.zemek@previousnext.com.au>
2015-08-07 13:54:28 +1000

    Added additional methods to factory

--- src/StaticReflectionFactory.php
+++ src/StaticReflectionFactory.php
@@ -39,12 +39,14 @@ class StaticReflectionFactory {
     }
 
     /**
+     * Get ReflectionClass.
+     *
      * @param string $className
      *   Fully qualified class name.
      *
-     * @return \ReflectionClass
+     * @return StaticReflectionClass
      *   Reflection class.
-     * 
+     *
      * @throws \ReflectionException
      *   Class not found.
      */
@@ -64,10 +66,60 @@ class StaticReflectionFactory {
         return $this->classes[$className];
     }
 
+    /**
+     * Unload StaticReflectionClass from factory cache.
+     *
+     * @param string $className
+     *   Fully qualified class name.
+     */
+    public function unloadClass($className) {
+        unset($this->classes[$className]);
+    }
+
+    /**
+     * Parse file.
+     *
+     * @param string $filename
+     *   Filename to parse.
+     */
     public function parseFile($filename)
     {
         $this->parser->parseFile($filename);
         $this->classes += $this->parser->getClasses();
+        $this->functions += $this->parser->getFunctions();
         $this->parser->clear();
+    }
+
+    /**
+     * Get ReflectionFunction.
+     *
+     * There is no autoload mechanism for functions so call ::parseFile()
+     * on file containing function first.
+     *
+     * @param string $functionName
+     *   Fully qualified function name.
+     *
+     * @return StaticReflectionFunction
+     *   Reflection function.
+     *
+     * @throws \ReflectionException
+     *   Function not found.
+     */
+    public function getFunction($functionName)
+    {
+        if (!isset($this->functions[$functionName])) {
+            throw new \ReflectionException("Function $functionName() does not exist");
+        }
+        return $this->functions[$functionName];
+    }
+
+    /**
+     * Unload StaticReflectionFunction from factory cache.
+     *
+     * @param string $functionName
+     *   Fully qualified function name.
+     */
+    public function unloadFunction($functionName) {
+        unset($this->functions[$functionName]);
     }
 }
